563b73e7135bc7cf8af3a27822dfb49c481f4a3b
dayasouza <dayanalvesouza@gmail.com>
2022-09-26 20:43:39 -0300

    Exercício 2
    Commit do exercicio 2 da aula 2

--- ProjetoAvaliacao/src/ADS/Principal.java
+++ ProjetoAvaliacao/src/ADS/Principal.java
@@ -3,18 +3,12 @@ package ADS;
 public class Principal {
 
 	public static void main(String[] args) {
-		Avaliacao luigi = new Avaliacao();
-		Avaliacao mario = new Avaliacao(7,4,10);
-		luigi.n1 = 8;
-		luigi.n2 = 3;
-		luigi.n3 = 7.5;
 		
-		System.out.println("Média Aritmetica do Mario: " + mario.mediaAritmetica());
-		System.out.println("Media Ponderada do Mario: " + mario.mediaPonderada());
+		Aluno a1 = new Aluno("Mario", "Encanador", new Avaliacao(9,10,3));
+		Aluno a2 = new Aluno("Luigi", "Encanador", new Avaliacao(4,1,10));
 		
-		System.out.println("Média Aritmetica do Luigi: " + luigi.mediaAritmetica());
-		System.out.println("Media Ponderada do Luigi: " + luigi.mediaPonderada());
-
+		a1.info();
+		a2.info();
 	}
 
 }
