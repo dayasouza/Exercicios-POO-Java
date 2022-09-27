79da0a05ad15de363176ded1024ac0b349d3fd96
dayasouza <dayanalvesouza@gmail.com>
2022-09-27 20:41:24 -0300

    LocalDate
    Explicações em Aula de POO sobre LocalDate

---
+++ Data/src/Main.java
@@ -0,0 +1,14 @@
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+public class Main {
+    public static void main(String[] args) {
+
+        LocalDate dataHoje = LocalDate.now();
+
+        System.out.println(dataHoje);
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy - EEEE");
+        System.out.println(dataHoje.format(formatador));
+
+    }
+}
