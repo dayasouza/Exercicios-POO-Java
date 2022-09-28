fd65a1814f496f1b47330c3b4584b5aeec5827ce
dayasouza <dayanalvesouza@gmail.com>
2022-09-28 14:41:36 -0300

    Cofrinho
    Inserido ultimo exercício aula prática de POO Cofrinho de moedas

---
+++ Cofrinho_EX3/src/Cofrinho.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public class Cofrinho {
+    private ArrayList<Moeda> moedas = new ArrayList<>();
+    
+    void add(Moeda m){
+        moedas.add(m);
+    }
+
+    public double calcularTotal() {
+        double total = 0;
+        for(Moeda m : moedas){
+            total += m.getValor();
+        }
+        return total;
+    }
+
+
+}
