abd92c661547c6f6b8ce9873de4a2a2d56b67359
dayasouza <dayanalvesouza@gmail.com>
2022-09-28 11:03:42 -0300

    Adicionado atributo Faltas
    Foi adicionado ao programa Notas o atributo faltas, onde, caso o aluno tenha + que 7 faltas ele é reprovado por faltas, independente das notas.

--- Notas/src/Main.java
+++ Notas/src/Main.java
@@ -2,12 +2,21 @@ public class Main {
     public static void main(String[] args) {
 
         Nota mario = new Nota();
+
+        Nota luigi = new Nota(2,5,5);
+
+        System.out.println("Média Luigi");
+        luigi.resultado();
+        System.out.println();
+
         mario.setNota1(9);
         mario.setNota2(10);
+        mario.setFaltas(5);
 
         mario.resultado();
 
         mario.setNota1(2);
+        mario.setFaltas(10);
 
         mario.resultado();
     }
--- Notas/src/Nota.java
+++ Notas/src/Nota.java
@@ -1,6 +1,16 @@
 public class Nota {
     private double nota1;
     private double nota2;
+    private int faltas;
+    public Nota(){
+
+    }
+
+    public Nota(double nota1, double nota2, int faltas) {
+        setNota1(nota1);
+        setNota2(nota2);
+        setFaltas(faltas);
+    }
 
     public void setNota1(double nota){
         if(nota < 0 || nota > 10){
@@ -16,6 +26,19 @@ public class Nota {
         }
         nota2 = nota;
     }
+
+    public int getFaltas() {
+        return faltas;
+    }
+
+    public void setFaltas(int faltas) {
+        if(faltas >40 || faltas <0){
+            System.out.println("Numero de faltas inválido");
+            return;
+        }
+        this.faltas = faltas;
+    }
+
     public double getNota1(){
         return nota1;
     }
@@ -29,8 +52,11 @@ public class Nota {
         double media = (nota1 + nota2) / 2;
 
         System.out.println("Média: " + media);
-        if(media <4){
-            System.out.println("Reprovado");
+        if(faltas >7){
+            System.out.println("Reprovado por faltas");
+        }
+        else if(media <4){
+            System.out.println("Reprovado por notas");
         }
         else if(media <7){
             System.out.println("Exame Final");
