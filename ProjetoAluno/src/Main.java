53ae82dd4d9c9cfde740bdfaad4caaa58854c762
dayasouza <dayanalvesouza@gmail.com>
2022-09-28 11:19:43 -0300

    Exercício 3 da aula prática de POO
    Adicionado o exercício 3 da aula prática 3 de POO em java

---
+++ ProjetoAluno/src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        Aluno a1 = new Aluno("Mario", 111, 0.1, new Curso("Encanador",1000));
+
+        a1.info();
+        //System.out.println("Pagamento: " + a1.pagamento());
+    }
+}
