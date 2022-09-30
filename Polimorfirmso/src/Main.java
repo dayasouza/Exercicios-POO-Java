8d8a64d7fc96eea3d485945fa92dfc86907947a3
dayasouza <dayanalvesouza@gmail.com>
2022-09-30 18:04:59 -0300

    Professor explicou como chamar varias classes

--- Polimorfirmso/src/Main.java
+++ Polimorfirmso/src/Main.java
@@ -1,15 +1,8 @@
+import java.util.HashMap;
+
 public class Main {
     public static void main(String[] args) {
-        Funcionario f1 = new Funcionario("Mario");
-        Funcionario f2 = new Funcionario("Mario");
-
-        System.out.println("Nome f1: " + f1.nome);
-        System.out.println("Nome f2: " + f2.nome);
-
-        System.out.println();
-        f2.nome = "Luigi";
-
-        System.out.println("Nome f1: " + f1.nome);
-        System.out.println("Nome f2: " + f2.nome);
+        Funcionario f1 = new Assalariado("Mario", 3500);
+        f1 = new Horista("Luigi", 100, 40.5f);
     }
 }
