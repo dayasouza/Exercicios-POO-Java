2389fe4bfcf499cdb4ee93029928060853853dcb
dayasouza <dayanalvesouza@gmail.com>
2022-09-30 18:15:13 -0300

    Criando um array de funcionarios

--- Polimorfirmso/src/Main.java
+++ Polimorfirmso/src/Main.java
@@ -2,7 +2,17 @@ import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        Funcionario f1 = new Assalariado("Mario", 3500);
-        f1 = new Horista("Luigi", 100, 40.5f);
+        Funcionario funcionarios[] = {new Assalariado("Mario",3500),
+        new Horista("Luigi", 100,40.5f),
+        new Comissionado("Yoshi", 50000, 0.05f)};
+
+        Funcionario f;
+        float total = 0;
+        for(int i=0; i<funcionarios.length; i++) {
+            f = funcionarios[i];
+            System.out.println(f.nome + " salario: " + f.pagamento());
+            total += f.pagamento();
+        }
+        System.out.println("Total: " + total);
     }
 }
