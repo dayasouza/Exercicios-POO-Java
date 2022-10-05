466e0ee261a4f5a80a83bfd2aa205f68f706cafc
dayasouza <dayanalvesouza@gmail.com>
2022-10-05 19:44:06 -0300

    Singleton

---
+++ Singleton/src/Singleton.java
@@ -0,0 +1,15 @@
+public class Singleton {
+    public int numero;
+    private static Singleton instancia = null;
+
+    private Singleton(){
+        numero = 20;
+    }
+
+    public static Singleton getInstance(){
+        if(instancia == null){
+            instancia = new Singleton();
+        }
+        return instancia;
+    }
+}
