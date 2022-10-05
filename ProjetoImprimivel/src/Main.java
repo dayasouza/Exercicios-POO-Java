4c54e0b953bdd1a8ad0660475ecd85903d2394c5
dayasouza <dayanalvesouza@gmail.com>
2022-10-05 17:10:06 -0300

    Usando interface
    Criando uma interface de coisas diferentes que se interligam em uma mesma interface

---
+++ ProjetoImprimivel/src/Main.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Funcionario f = new Funcionario("Mario", "111.222.333-44");
+        Carro c = new Carro("Palio","Cinza", 4);
+        Quadrado q = new Quadrado(10);.
+
+        ArrayList<Imprimivel> lista = new ArrayList<>();
+        lista.add(q);
+        lista.add(c);
+        lista.add(f);
+
+
+
+
+    }
+}
