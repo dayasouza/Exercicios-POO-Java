755b0ab8ce6f1a0c25ec048bb14f95165320c8b1
dayasouza <dayanalvesouza@gmail.com>
2022-10-05 16:41:45 -0300

    Projeto computador
    Criando o projeto computador, usando polimorfismo.

--- Projeto Livraria/src/Main.java
+++ Projeto Livraria/src/Main.java
@@ -1,14 +1,22 @@
 public class Main {
     public static void main(String[] args) {
-        LivroDigital ld = new LivroDigital("Senhor dos Aneis",
+        LivroDigital livroA = new LivroDigital("Senhor dos Aneis",
                 new Autor("Tolkien", "Britanico","tolkien@email.com"),
                         "Aventura",5, 10000,3500 );
-        ld.info();
+
         System.out.println();
-        LivroFisico lf= new LivroFisico("As crônicas de Gelo e Fogo",
+        LivroFisico livroB= new LivroFisico("As crônicas de Gelo e Fogo",
                 new Autor("George Martin", "Britanico","martin.george@email.com"),
                 "Aventura",3, 8000,5000 );
-        lf.info();
+
+        Livro  livro;
+        livro = livroA;
+        livro.info();
+        System.out.println("------------------------------");
+        livro = livroB;
+        livro.info();
+
+
 
     }
 }
