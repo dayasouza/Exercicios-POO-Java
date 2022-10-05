59f0a87319430474bb71427e3f059c2733b4f506
dayasouza <dayanalvesouza@gmail.com>
2022-10-05 19:29:47 -0300

    toString

--- Exemplo/src/Usuario.java
+++ Exemplo/src/Usuario.java
@@ -12,6 +12,13 @@ public class Usuario {
     }
 
     @Override
+    public String toString() {
+        return "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", cpf='" + cpf + '\'';
+    }
+
+    @Override
     public int hashCode() {
         final int prime = 31;
         int result = 1;
